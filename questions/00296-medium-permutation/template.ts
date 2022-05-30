d768b3a7b3dd3b82789658abdf0c4ddf0621eeab
refcell <2238932729@qq.com>
2022-05-30 18:41:20 +0800

    never aka empty union

--- questions/00296-medium-permutation/template.ts
+++ questions/00296-medium-permutation/template.ts
@@ -1,1 +1,6 @@
-type Permutation<T> = any
+// https://stackoverflow.com/questions/53984650/typescript-never-type-inconsistently-matched-in-conditional-type
+type Permutation<T, K = T> = [T] extends [never]
+  ? []
+  : T extends any
+    ? [T, ...Permutation<Exclude<K, T>>]
+    : never
