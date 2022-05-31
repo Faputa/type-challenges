24758b6d9e968e5ebd0cd0effe3d3a70f0ca37f1
refcell <2238932729@qq.com>
2022-05-31 22:42:50 +0800

    今日练习

--- questions/00114-hard-camelcase/template.ts
+++ questions/00114-hard-camelcase/template.ts
@@ -1,1 +1,3 @@
-type CamelCase<S extends string> = any
+type UppercaseFirst<S extends string> = S extends `${infer H}${infer T}` ? `${Uppercase<H>}${Lowercase<T>}` : `${Uppercase<S>}`
+type UpperCamelCase<S extends string> = S extends `${infer H}_${infer T}` ? `${UpperCamelCase<H>}${UpperCamelCase<T>}` : `${UppercaseFirst<S>}`
+type CamelCase<S extends string> = S extends `${infer H}_${infer T}` ? `${Lowercase<H>}${UpperCamelCase<T>}` : Lowercase<S>
