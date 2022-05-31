e6df509db33df5812027dcc8362ee1a126f695d1
refcell <2238932729@qq.com>
2022-05-31 22:53:54 +0800

    更好的实现

--- questions/00114-hard-camelcase/template.ts
+++ questions/00114-hard-camelcase/template.ts
@@ -1,3 +1,4 @@
-type UppercaseFirst<S extends string> = S extends `${infer H}${infer T}` ? `${Uppercase<H>}${Lowercase<T>}` : `${Uppercase<S>}`
-type UpperCamelCase<S extends string> = S extends `${infer H}_${infer T}` ? `${UpperCamelCase<H>}${UpperCamelCase<T>}` : `${UppercaseFirst<S>}`
-type CamelCase<S extends string> = S extends `${infer H}_${infer T}` ? `${Lowercase<H>}${UpperCamelCase<T>}` : Lowercase<S>
+// type UppercaseFirst<S extends string> = S extends `${infer H}${infer T}` ? `${Uppercase<H>}${Lowercase<T>}` : `${Uppercase<S>}`
+// type UpperCamelCase<S extends string> = S extends `${infer H}_${infer T}` ? `${UpperCamelCase<H>}${UpperCamelCase<T>}` : `${UppercaseFirst<S>}`
+// type CamelCase<S extends string> = S extends `${infer H}_${infer T}` ? `${Lowercase<H>}${UpperCamelCase<T>}` : Lowercase<S>
+type CamelCase<S extends string> = S extends `${infer H}_${infer T}` ? `${Lowercase<H>}${Capitalize<CamelCase<T>>}` : Lowercase<S>
