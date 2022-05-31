c8fe76386115ea0e5c2794fc70a59ab1e6706dc8
refcell <2238932729@qq.com>
2022-05-31 21:48:08 +0800

    可变参数元组类型Variadic tuple types

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,3 @@
-declare function PromiseAll(values: any): any
+// type hh<T> = T extends [infer A, ...infer B] ? [A extends Promise<infer V> ? V : A, ...hh<B>] : []
+// declare function PromiseAll<T extends any[]>(values: readonly [...T]): Promise<hh<T>>
+declare function PromiseAll<T extends any[]>(values: readonly [...T]): Promise<{ [K in keyof T]: T[K] extends Promise<infer V> ? V : T[K] }>
