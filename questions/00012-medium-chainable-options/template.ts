157d22089fe2e2369d211f041b63d0294780597b
Faputa <2238932729@qq.com>
2022-06-17 16:08:16 +0800

    完善12题的解答

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,17 +1,4 @@
-// https://github.com/type-challenges/type-challenges/issues/9052
-// type OmitUnion<T, K> = T extends K ? never : T
-// type EmptyObject<T> = keyof T extends keyof {} ? true : false
-// type Chainable<T extends {} = {}> = {
-//   option<K extends
-//   EmptyObject<T> extends true
-//     ? string
-//     : OmitUnion<keyof T, K> extends true
-//       ? never
-//       : string
-//   , V>(key: K, value: V): Chainable<T & { [k in K]: V }>
-//   get(): T
-// }
 type Chainable<T extends {} = {}> = {
-  option<K extends string, V>(key: K, value: V): Chainable<T & { [N in K]: V }>
+  option<K extends string, V>(key: K extends keyof T ? never : K, value: V): Chainable<T & { [N in K]: V }>
   get(): T
 }
