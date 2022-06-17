2d01fa7dcac88d3365d858a413f943e467b0e81e
Faputa <2238932729@qq.com>
2022-06-18 03:27:21 +0800

    简单的Vue类型

--- questions/00006-hard-simple-vue/template.ts
+++ questions/00006-hard-simple-vue/template.ts
@@ -1,1 +1,9 @@
-declare function SimpleVue(options: any): any
+// 魔法类型ThisType
+type Computed<C> = { [K in keyof C]: C[K] extends () => infer R ? R : never }
+type Options<D, C, M> = {
+  data(): D
+  computed: C & ThisType<M & D & Computed<C>>
+  methods: M & ThisType<M & D & Computed<C>>
+} & ThisType<never>
+declare function SimpleVue<D, C, M>(options: Options<D, C, M>): any
+declare function alert(arg: any): any
