cdc496fd29622718175be6afcc45c0a1bbff22f9
Faputa <2238932729@qq.com>
2022-06-18 18:37:08 +0800

    更好的实现

--- questions/00006-hard-simple-vue/template.ts
+++ questions/00006-hard-simple-vue/template.ts
@@ -1,9 +1,9 @@
 // 魔法类型ThisType
 type Computed<C> = { [K in keyof C]: C[K] extends () => infer R ? R : never }
 type Options<D, C, M> = {
-  data(): D
-  computed: C & ThisType<M & D & Computed<C>>
-  methods: M & ThisType<M & D & Computed<C>>
-} & ThisType<never>
+  data(this: {}): D
+  computed: C
+  methods: M
+} & ThisType<M & D & Computed<C>>
 declare function SimpleVue<D, C, M>(options: Options<D, C, M>): any
 declare function alert(arg: any): any
