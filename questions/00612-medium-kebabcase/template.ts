ee6e20d94e61b055bc09f7dda7ef91be46310a42
Faputa <2238932729@qq.com>
2022-06-19 19:43:41 +0800

    今日练习

--- questions/00612-medium-kebabcase/template.ts
+++ questions/00612-medium-kebabcase/template.ts
@@ -1,1 +1,2 @@
-type KebabCase<S> = any
+type _KebabCase<S extends string> = S extends `${infer A}${infer B}` ? A extends Lowercase<A> ? `${A}${_KebabCase<B>}` : `-${Lowercase<A>}${_KebabCase<B>}` : S
+type KebabCase<S extends string> = _KebabCase<S extends `${infer A}${infer B}` ? `${Lowercase<A>}${B}` : S>
--- questions/00645-medium-diff/template.ts
+++ questions/00645-medium-diff/template.ts
@@ -1,1 +1,4 @@
-type Diff<O, O1> = any
+type _Diff<O, O1> =
+  { [K in Exclude<keyof (O & O1), keyof O>]: O1[K] } &
+  { [K in Exclude<keyof (O & O1), keyof O1>]: O[K] }
+type Diff<O, O1, D = _Diff<O, O1>> = { [K in keyof D]: D[K] }
