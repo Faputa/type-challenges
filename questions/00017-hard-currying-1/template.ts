046f752c1569ef656938f9949eb95cea5751db94
Faputa <2238932729@qq.com>
2022-06-21 14:41:13 +0800

    柯里化 1

--- questions/00017-hard-currying-1/template.ts
+++ questions/00017-hard-currying-1/template.ts
@@ -1,1 +1,2 @@
-declare function Currying(fn: any): any
+type Currying<P, R> = P extends [infer A, ...infer B] ? (a: A) => Currying<B, R> : R
+declare function Currying<F>(fn: F): F extends (...args: infer P) => infer R ? Currying<P, R> : never
