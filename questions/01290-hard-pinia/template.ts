53b533f4edeeced540deb46444fbea2e4b8aa008
Faputa <2238932729@qq.com>
2022-06-21 13:20:26 +0800

    Pinia

--- questions/01290-hard-pinia/template.ts
+++ questions/01290-hard-pinia/template.ts
@@ -1,1 +1,7 @@
-declare function defineStore(store: unknown): unknown
+type Getters<G> = { readonly [K in keyof G]: G[K] extends () => infer T ? T : never }
+declare function defineStore<S, G, A>(store: {
+  id: string
+  state(): S
+  getters: G & ThisType<Readonly<S> & Getters<G>>
+  actions: A & ThisType<S & Getters<G> & A>
+}): S & Getters<G> & A
