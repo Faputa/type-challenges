9d98af89f6821bac36076df9242ee0f8da77993f
Faputa <2238932729@qq.com>
2022-06-22 13:21:07 +0800

    Tuple Filter

--- questions/00399-hard-tuple-filter/template.ts
+++ questions/00399-hard-tuple-filter/template.ts
@@ -1,1 +1,3 @@
-type FilterOut<T extends any[], F> = any
+type FilterOut<T extends any[], F> = T extends [infer A, ...infer B]
+  ? [A] extends [F] ? FilterOut<B, F> : [A, ...FilterOut<B, F>]
+  : T
