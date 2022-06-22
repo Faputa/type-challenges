c03de3f49e678c94b712149dd6d8ac8de8a5884b
Faputa <2238932729@qq.com>
2022-06-22 16:44:04 +0800

    Tuple to Enum Object

--- questions/00472-hard-tuple-to-enum-object/template.ts
+++ questions/00472-hard-tuple-to-enum-object/template.ts
@@ -1,1 +1,6 @@
-type Enum<T extends readonly string[], N extends boolean = false> = any
+type _IndexOf<T, U, V extends any[] = []> = T extends readonly [infer A, ...infer B]
+  ? U extends A ? V['length'] : IndexOf<B, U, [...V, A]>
+  : -1
+type Enum<T extends readonly string[], N extends boolean = false> = {
+  readonly [K in T[number]as Capitalize<K>]: N extends true ? _IndexOf<T, K> : K
+}
