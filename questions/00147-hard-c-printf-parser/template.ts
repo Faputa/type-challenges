324c59905a18fab504b1e0d24e458a6e6028b15e
Faputa <2238932729@qq.com>
2022-06-22 10:26:02 +0800

    C-printf Parser

--- questions/00147-hard-c-printf-parser/template.ts
+++ questions/00147-hard-c-printf-parser/template.ts
@@ -8,4 +8,6 @@ type ControlsMap = {
   p: 'pointer'
 }
 
-type ParsePrintFormat = any
+type ParsePrintFormat<S extends string, C extends string[] = []> = S extends `${infer _}%${infer A}${infer B}`
+  ? ParsePrintFormat<B, A extends keyof ControlsMap ? [...C, ControlsMap[A]] : C>
+  : C
