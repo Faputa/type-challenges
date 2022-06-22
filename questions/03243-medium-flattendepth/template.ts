614497ed45c9644f724f2a42b894623e1a1f1c23
Faputa <2238932729@qq.com>
2022-06-22 19:05:11 +0800

    FlattenDepth

--- questions/03243-medium-flattendepth/template.ts
+++ questions/03243-medium-flattendepth/template.ts
@@ -1,1 +1,6 @@
-type FlattenDepth = any
+type FlattenDepth<T, U = 1, V extends any[] = []> = U extends V['length'] ? T
+  : T extends [infer A, ...infer B]
+    ? A extends any[]
+      ? [...FlattenDepth<A, U, [...V, 1]>, ...FlattenDepth<B, U, V>]
+      : [A, ...FlattenDepth<B, U, V>]
+    : []
